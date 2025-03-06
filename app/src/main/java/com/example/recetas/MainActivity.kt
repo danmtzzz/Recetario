d853a6a6e1da0df05acdfca65c5753afa7720973
danmtzzz <alu.22130529@correo.itlalaguna.edu.mx>
2025-03-06 13:25:01 -0600

    Initial Commit - Recetario

---
+++ app/src/main/java/com/example/recetas/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.recetas
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.recetas.navigation.NavManager
+import com.example.recetas.ui.theme.RecetasTheme
+import com.example.recetas.views.HomeView
+import com.example.recetas.views.Receta1
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+                    //Receta1()
+//            HomeView()
+            GreetingPreview()
+                }
+            }
+        }
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview(){
+    NavManager()
+}
+
